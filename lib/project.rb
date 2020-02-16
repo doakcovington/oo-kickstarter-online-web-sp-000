8f0ae5d4f11839c0db872385fabb1320025d3fa9
Doak Covington <doakcovington@protonmail.com>
2020-02-16 22:35:17 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   end
 
   def add_backer(backer)
-    @backers << backer #A project has many backers
+    backers << backer #A project has many backers
     backer.backed_projects << self#belongs to
   end
 
